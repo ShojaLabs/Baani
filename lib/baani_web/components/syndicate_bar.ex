47af0a892594848788c85a27be5849d80dd25789
rangrik <pranav@shoja.ai>
2024-11-23 18:01:32 -0800

    feat(home): simplify home page layout and content can be scrolled

--- lib/baani_web/components/syndicate_bar.ex
+++ lib/baani_web/components/syndicate_bar.ex
@@ -9,7 +9,7 @@ defmodule BaaniWeb.SyndicateBar do
   def navbar(assigns) do
     ~H"""
     <%= if @current_user do %>
-      <div class="card bg-base-100 w-60 min-h-full rounded-none border-r border-base-200 pr-2">
+      <div class="card bg-base-100 w-60 min-h-full rounded-none border-r border-base-200 pr-2 bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] shadow-sm">
         <button class="btn">Click me</button>
       </div>
     <% end %>
