c200f6131cb880d3d6c3cccab5a186abab2f1ab6
rangrik <pranav@shoja.ai>
2024-11-25 14:11:16 -0800

    can conditionally show the login/register buttons

--- lib/baani_web/components/navbar.ex
+++ lib/baani_web/components/navbar.ex
@@ -8,9 +8,15 @@ defmodule BaaniWeb.Navbar do
 
   def navbar(assigns) do
     ~H"""
-    <div class="relative navbar min-h-fit p-2 bg-base-200 rounded-lg">
+    <div class={[
+      "relative navbar p-2 bg-base-200 rounded-lg",
+      (@current_user && "min-h-fit") || "px-4"
+    ]}>
       <div class="navbar-start">
-        <.link href={~p"/"} class="btn btn-ghost btn-sm">
+        <.link
+          href={~p"/"}
+          class="link link-hover btn-sm flex items-center gap-2 text-lg font-semibold px-0"
+        >
           <.icon name="hero-microphone-solid" class="w-4 h-4" /> Baani
         </.link>
       </div>
@@ -18,12 +24,24 @@ defmodule BaaniWeb.Navbar do
         <%= if @current_user do %>
           <BaaniWeb.ProfileMenu.menu current_user={@current_user} />
         <% else %>
-          <.link href={~p"/users/register"} class="btn btn-primary btn-sm">
-            Register
-          </.link>
-          <.link href={~p"/users/log_in"} class="btn btn-ghost btn-sm">
-            Log in
-          </.link>
+          <%= unless @conn.request_path == ~p"/users/log_in" do %>
+            <.link href={~p"/users/log_in"} class={["btn btn-outline"]}>
+              Login
+            </.link>
+          <% end %>
+          <%= unless @conn.request_path == ~p"/users/register" do %>
+            <.link
+              href={~p"/users/register"}
+              class={[
+                "btn",
+                ((@conn.request_path == ~p"/users/log_in" ||
+                    @conn.request_path == ~p"/users/register") && "btn-outline") ||
+                  "btn-primary"
+              ]}
+            >
+              Register
+            </.link>
+          <% end %>
         <% end %>
       </div>
     </div>
