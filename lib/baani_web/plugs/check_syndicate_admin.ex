082a426e3d563312e1d4a4731ae93c22235c1c46
rangrik <pranav@shoja.ai>
2025-01-10 13:27:07 +0530

    feat: enhance error handling in CheckSyndicateAdmin plug

--- lib/baani_web/plugs/check_syndicate_admin.ex
+++ lib/baani_web/plugs/check_syndicate_admin.ex
@@ -2,6 +2,7 @@ defmodule BaaniWeb.Plugs.CheckSyndicateAdmin do
   import Plug.Conn
   import Phoenix.Controller
   alias Baani.Syndicates
+  require Logger
 
   def init(default), do: default
 
@@ -9,15 +10,29 @@ defmodule BaaniWeb.Plugs.CheckSyndicateAdmin do
     user_id = conn.assigns.current_user.id
     syndicate_id = conn.params["id"]
 
-    case Syndicates.get_syndicate!(syndicate_id, user_id) do
-      %{is_admin: true} ->
-        conn
+    try do
+      case Syndicates.get_syndicate!(syndicate_id, user_id) do
+        %{is_admin: true} ->
+          conn
 
-      _ ->
-        conn
-        |> put_flash(:error, "You are not authorized to access this page.")
-        |> redirect(to: "/syndicates")
-        |> halt()
+        _ ->
+          unauthorized_access(conn)
+      end
+    rescue
+      Ecto.NoResultsError ->
+        Logger.error("Syndicate not found for ID: #{syndicate_id}")
+        unauthorized_access(conn)
+
+      error ->
+        Logger.error("An error occurred: #{inspect(error)}")
+        unauthorized_access(conn)
     end
+  end
+
+  defp unauthorized_access(conn) do
+    conn
+    |> put_flash(:error, "You are not authorized to access this page.")
+    |> redirect(to: "/syndicates")
+    |> halt()
   end
 end
